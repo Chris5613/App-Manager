532d9d1a1996656568f6ae61f4d9866412e2d930
Christian Wu <christianwu56@gmail.com>
2023-01-17 20:00:15 -0800

    Created views for the apis

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from .models import Task
+from .serializers import TaskSerializer
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+@api_view(['GET','POST'])
+def task_list(request):
+
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET','DELETE'])
+def task_details(request,id):
+
+    try:
+        tasks = Task.objects.get(id=id)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    if request.method == "GET":
+        serializer = TaskSerializer(tasks)
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        tasks.delete()
+        return JsonResponse(status=status.HTTP_200_OK)
