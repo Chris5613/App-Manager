556c7387e1a9edb1c3a235a20648203ecb712c6d
Christian Wu <christianwu56@gmail.com>
2023-01-19 20:22:40 -0800

    Deleted user auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './css/taskbuddy.css';
 import './css/skyscope.css';
 import App from './App';
-import Login from './components/TaskBuddy/UserAuth/Login';
-import Signup from './components/TaskBuddy/UserAuth/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,11 +10,3 @@ root.render(
     <App />
 </React.StrictMode>
 );
-
-// const div = ReactDOM.createRoot(document.getElementById('contain'))
-// div.render(
-// <>
-// <Login />
-// <Signup />
-// </>
-// )
