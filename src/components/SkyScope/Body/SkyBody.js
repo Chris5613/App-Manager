9014db1b0d633943235a1dac1a712f39e958881b
Christian Wu <christianwu56@gmail.com>
2023-03-16 04:20:17 +0000

    changed api keys

--- src/components/SkyScope/Body/SkyBody.js
+++ src/components/SkyScope/Body/SkyBody.js
@@ -5,7 +5,7 @@ import UpperBody from './UpperBody';
 const Body = () => {
     const [data, setData] = useState({})
     const [location, setLocation] = useState('')
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=bd0a7e15427ee62b3be173092dc5feea`
 
     const searchLocation = (event) => {
     if (event.key === 'Enter') {
